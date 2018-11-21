f29ea3de38fadf10c676ce9ad764bcf1a27394a3
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2018-11-20 18:01:58 -0800

    new pipeline: extract tweet data from raw JSON

---
+++ pipelines/0_json/_helpers.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import json
+
+import numpy as np
+import pandas as pd
+import urltools
+
+
+# Loading individual tweets and manipulate them
+def load_json(x):
+    '''
+    Load Tweet JSON
+    '''
+    try:
+        return json.loads(x, strict=False)
+    except:
+        print("Problematic tweet found.")
+        return None
+
+
+def get_tweet_urls(t):
+    '''
+    Given a Tweet JSON, pull the URLs found inside it
+    '''
+    try:
+        return get_urls(t['entities']['urls'])
+    except:
+        return []
+
+
+def get_retweet_urls(t):
+    '''
+    Given a Tweet JSON, pull the URLs of the Tweet this tweet retweeted
+    '''
+    try:
+        return get_urls(t['retweeted_status']['entities']['urls'])
+    except:
+        return []
+
+
+def get_urls(urls):
+    '''
+    Generic function to extract the URLs from the urls sub-object
+    '''
+    try:
+        urls = [v for (k, v) in urls[0].items()
+                if k in ('url', 'expanded_url')]
+        return list(set(urls))
+    except:
+        return []
+
+
+# Loading files
+def load_urls(file):
+    '''
+    Load file that contains tweets & urls
+    '''
+    urls = pd.read_csv(file,
+                       na_values="None",
+                       dtype={'tweet_id': str,
+                              'retweet_id': str,
+                              'relevant_url': str,
+                              'clean_url': str})
+    urls = urls.drop_duplicates()
+    urls = urls.set_index("tweet_id")
+    return urls
+
+
+def load_queries(file):
+    '''
+    Load file that contains information about the search queries
+    '''
+    queries = pd.read_csv(file, index_col="id")
+    return queries
+
+
+# URL operations
+def clean_url(url, venue=None):
+    '''
+    Strip out trailing slashes, URL query variables, anchors, etc.
+    '''
+    if pd.isna(url):
+        return np.nan
+
+    # remove parts of the URL that come before the domain (e.g., google news)
+    if venue:
+        url = "www." + venue + "".join(url.split(venue)[1:])
+
+    try:
+        up = urltools.extract(url)
+        url = up.domain + "." + up.tld + up.path
+        url = urltools.normalize(url)
+        return url
+    except:
+        raise
+
+
+def relevant_url(url, terms):
+    '''
+    Check if URL contains one of the search terms for each news venue
+    in the form "/search_term/" (which would typically be a section)
+    '''
+    if pd.isna(url):
+        return False
+
+    for term in terms:
+        if "/" + term + "/" in url:
+            return True
+    return False
+
+
+def merge_urls(row):
+    '''
+    If relevant URL exists return it, otherwise
+    use retweeted URL
+    '''
+    if not pd.isna(row['relevant_url']):
+        return row['relevant_url']
+    return row['retweet_url']
---
+++ pipelines/0_json/extract_tweet_data.py
@@ -0,0 +1,81 @@
+import csv
+from pathlib import Path
+import pandas as pd
+from dateutil.parser import parse
+from tqdm import tqdm
+
+from _helpers import *
+
+
+if __name__ == "__main__":
+    headers = ['tweet_id', 'posted_on', 'user_id', 'retweeted_status',
+               'quoted_status', 'in_reply_to', 'urls', 'is_truncated']
+
+    # Load config
+    logger.info('Loading configuration.')
+    root = Path('../../')
+    Config = configparser.ConfigParser()
+    Config.read(str(root / 'config.cnf'))
+
+    # Load files from disk
+    queries = root / Config.get('input_files', 'raw')
+    input_dir = root / Config.get('input_files', 'raw_tweets')
+    output_dir = root / Config.get('output_files', 'tweets')
+
+    queries = load_queries(str(queries))
+
+    files = input_dir.glob("*.csv")
+    for infile in files:
+        outfile = output_dir / infile.name
+        if outfile.exists():
+            print("{} already exists. skipping".format(outfile))
+            continue
+
+        query_name = infile.name.split(".")[0]
+        row_count = queries.loc[queries['query'] == query_name, "found_tweets"].iloc[0]
+
+        print("Collecting {}".format(infile.name))
+        with open(str(infile), "r") as inf:
+            with open(str(outfile), "a") as outf:
+                reader = csv.reader(inf)
+                next(reader, None)
+
+                writer = csv.writer(outf)
+                writer.writerow(headers)
+
+                for row in tqdm(reader, total=row_count):
+                    tweet = load_json(row[1])
+
+                    if not tweet:
+                        continue
+
+                    tweet_id = str(tweet['id_str'])
+                    posted_on = str(parse(tweet['created_at'], ignoretz=True))
+                    user_id = str(tweet['user']['id_str'])
+                    is_truncated = str(tweet['truncated'])
+
+                    urls = get_tweet_urls(tweet)
+                    if len(urls) > 0:
+                        urls = str(urls)
+                    else:
+                        urls = None
+
+                    if 'retweeted_status' in tweet:
+                        retweeted_status = str(tweet['retweeted_status']['id_str'])
+                    else:
+                        retweeted_status = None
+
+                    if 'quoted_status_id_str' in tweet:
+                        quoted_status = str(tweet['quoted_status_id_str'])
+                    else:
+                        quoted_status = None
+
+                    if tweet['in_reply_to_status_id_str']:
+                        in_reply_to = str(tweet['in_reply_to_status_id_str'])
+                    else:
+                        in_reply_to = None
+
+                    row = [tweet_id, posted_on, user_id, retweeted_status,
+                           quoted_status, in_reply_to, urls, is_truncated]
+
+                    writer.writerow(row)
