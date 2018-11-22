201d4f1db1931ac527682326c3aacea3bf9c3761
Asura Enkhbayar <asura.enkhbayar@gmail.com>
2018-11-21 21:16:00 -0800

    major rewrite

--- pipelines/2_urls/get_relevant_urls.py
+++ pipelines/2_urls/get_relevant_urls.py
@@ -59,12 +59,18 @@ class PublisherTracker(object):
 
 @sleep_and_retry
 @limits(calls=30, period=1)
-def resolve_url(url, session, timeout=5):
+def resolve_url(url, session, timeout=8):
     try:
         resp = session.get(url, allow_redirects=True, timeout=timeout)
         return resp.url, None
     except Exception as e:
         return None, e
+
+
+def exp_write_row(f, row):
+    with open(str(f), 'a') as f:
+        exp_writer = csv.writer(f)
+        exp_writer.writerow(row)
 
 
 # file headers
@@ -100,7 +106,7 @@ logger.addHandler(ch)
 
 if __name__ == "__main__":
     # Load config
-    logger.info('Loading configuration.')
+    logger.info('# Loading configuration.')
     root = Path('../../')
     Config = configparser.ConfigParser()
     Config.read(str(root / 'config.cnf'))
@@ -112,55 +118,61 @@ if __name__ == "__main__":
     input_files = root / Config.get('output_files', 'tweets')
 
     # Expanded URLs
-    logger.info("Trying to load previously expanded URLs and prepare file stream")
+    logger.info("# Trying to load previously expanded URLs")
     if exp_file.exists():
-        exp = pd.read_csv(str(exp_file), index_col="id")
+        exp = pd.read_csv(str(exp_file), index_col=0)
     else:
         exp = pd.DataFrame(columns=exp_url_headers)
-        exp.index.name = "id"
         exp.to_csv(str(exp_file), index=True)
 
-    g_resolved_urls = set(exp[exp.error.isnull()].index.tolist())
-    b_resolved_urls = set(exp[exp.error.notnull()].index.tolist())
+    # # Clean and update exp_urls on disk
+    # exp = exp.drop_duplicates(subset=['short_url', 'resolved_url'])
+    # exp.reset_index(drop=True)
+    # exp.to_csv(str(exp_file), index=True)
+
+    expanded_url_count = len(exp)
+
+    # Create dicts and sets for looping
+    good_exp_urls = {}
+    for row in exp[exp.error.isnull()][['short_url', 'resolved_url']].itertuples():
+        good_exp_urls[row[1]] = row[2]  # if multiple urls, we just take the latest resolved one
+    bad_exp_urls = set(exp[exp.error.notnull()].short_url.tolist())
 
     # Init publication tracker
     pub_tracker = PublisherTracker()
 
     files = input_files.glob("*.csv")
     for infile in files:
+        logger.info("## Processing {} ".format(infile.name))
         # Skip files that start with _ (IFLscience, Chicago-Suntimes)
         if infile.name[0] == "_":
-            logger.info("Skipping {}".format(infile.name))
+            logger.info("## Skipping {}")
             continue
 
         # Final output file
         outfile = output_dir / infile.name
         if outfile.exists():
-            appending_mode = True
-            previous_tweets = pd.read_csv(outfile)
-            logger.info("URLs for {} already exist. Appending new rows.".format(infile.name))
+            logger.info("## URLs already exist. Appending new rows.")
+            previous_tweets = pd.read_csv(str(outfile), dtype={'tweet_id': str})
         else:
-            appending_mode = False
+            logger.info("## URLs do not exist. Creating new file..")
             previous_tweets = pd.DataFrame(columns=outfile_headers)
-            logger.info("URLs for {} do not exist. Creating new file..".format(infile.name))
+            previous_tweets.to_csv(str(outfile), index=False)
 
-        session = requests.Session()
-
-        logger.info("Processing tweets in {}".format(infile.name))
+        previous_tweet_ids = set(previous_tweets.tweet_id.tolist())
+        logger.info("## Found {} relevant URLs".format(len(previous_tweet_ids)))
 
         # Open file stream
+        logger.info("## Creating file streams")
         infile = open(str(infile), "r")
-        outfile = open(str(outfile), "a")
-
         reader = csv.reader(infile)
-        next(reader, None)  # skip headers
-        count = sum(1 for _ in reader)  # count number of rows
+        count = sum(1 for _ in reader) - 1  # count number of rows minus header
         infile.seek(0)
-
         reader = csv.reader(infile)
+        next(reader, None)
+
+        outfile = open(str(outfile), "a")
         writer = csv.writer(outfile)
-        if not appending_mode:
-            writer.writerow(outfile_headers)
 
         # Load relevant terms
         query = infile.name.split("/")[-1].split(".")[0]
@@ -169,21 +181,25 @@ if __name__ == "__main__":
         terms = queries.groupby("venue_short")['relevant_terms'].apply(lambda x: list(x))
         terms = terms[venue_short]
 
+        session = requests.Session()
+
         for row in tqdm(reader, total=count):
             tweet_id = row[0]
 
-            if tweet_id in previous_tweets.tweet_id.tolist():
+            if tweet_id in previous_tweet_ids:
+                logger.debug("### {}: Tweet already has relevant URL.".format(tweet_id))
                 continue
 
             tweet = load_json(row[2])
             if not tweet:
-                logging.debug("Couldn't load tweet")
+                logging.debug("### {}: Couldn't load tweet".format(tweet_id))
                 continue
 
             try:
                 # Get tweet & retweet URLs
                 s = row[infile_headers.index('urls')]
                 if pd.isna(s) or s is "":
+                    logger.debug("### {}: Tweet contains no URLs.".format(tweet_id))
                     continue
                 url_candidates = json.loads(s)
 
@@ -195,60 +211,62 @@ if __name__ == "__main__":
 
                 for url in url_candidates:
                     if relevant_url(url, venue_short, terms):
+                        logger.debug("### {}: Found relevant link in tweet.".format(tweet_id))
                         found_url = url
                         expanded = False
                         break
 
                 if not found_url:
                     for url in url_candidates:
-                        if url in g_resolved_urls:
-                            df = exp[exp.short_url == url]
-                            df = df[df['error'].isnull()]
-                            url = df.iloc[0]["resolved_url"]
-                        elif url in b_resolved_urls:
-                            logger.debug("Resolving URL that previously failed.")
-                            r_url, error = resolve_url(url, session)
-                            if not error:
-                                g_resolved_urls.add(url)
-                                b_resolved_urls.remove(url)
-                            exp.loc[len(exp)+1] = [url, r_url, error, str(datetime.now())]
-                            with open(str(exp_file), 'a') as f:
-                                exp_writer = csv.writer(f)
-                                exp_writer.writerow(
-                                    [len(exp), url, r_url, error, str(datetime.now())])
-                            url = r_url                              
-                        else:
-                            logger.debug("New URL. Resolving.")
-                            r_url, error = resolve_url(url, session)
-                            if not error:
-                                g_resolved_urls.add(url)
-                            else:
-                                b_resolved_urls.add(url)
-                            exp.loc[len(exp)+1] = [url, r_url, error, str(datetime.now())]
-                            with open(str(exp_file), 'a') as f:
-                                exp_writer = csv.writer(f)
-                                exp_writer.writerow(
-                                        [len(exp), url, r_url, error, str(datetime.now())])
-                            url = r_url
+                        c_url, error = None, None
+                        expanded = False
 
-                        if relevant_url(url, venue_short, terms):
-                            found_url = url
+                        # Find candidate URL in existing data or with resolving
+                        if url in good_exp_urls:
+                            logger.debug("### {}: Reusing previous resolve.".format(tweet_id))
+                            c_url = good_exp_urls[url]
+                        elif url in bad_exp_urls:
+                            logger.debug("### {}: Resolving URL that previously failed.".format(tweet_id))
+                            c_url, error = resolve_url(url, session)
                             expanded = True
-                            break
+
+                            if c_url:
+                                good_exp_urls[url] = c_url
+                                bad_exp_urls.remove(url)
+
+                                row = [expanded_url_count, url, c_url, error, str(datetime.now())]
+                                exp_write_row(str(exp_file), row)
+                                expanded_url_count = expanded_url_count + 1
+                        else:
+                            logger.debug("### {}: New URL. Resolving.".format(tweet_id))
+                            c_url, error = resolve_url(url, session)
+                            expanded = True
+
+                            row = [expanded_url_count, url, c_url, error, str(datetime.now())]
+                            exp_write_row(str(exp_file), row)
+                            expanded_url_count = expanded_url_count + 1
+
+                            if c_url:
+                                good_exp_urls[url] = c_url
+                            else:
+                                bad_exp_urls.add(url)   
+
+                        if c_url:
+                            if relevant_url(c_url, venue_short, terms):
+                                found_url = c_url
+                                break
 
                 if found_url:
                     pub_tracker.check_url(found_url)
 
                     now = str(datetime.now())
                     writer.writerow([str(tweet_id), str(found_url), expanded, now])
-
+                    outfile.flush()
             except TooManyPublisherRequests:
                 logger.exception("Too many requests to publishers")
                 pub_tracker.save_csv(pub_tracker_file)
                 sys.exit(0)
-
-        # Write expanded URLs
+        # Close file streams
         infile.close()
         outfile.close()
-    f.close()
     pub_tracker.save_csv(pub_tracker_file)
